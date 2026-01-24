6f1370a0715538c0c923ba19adbe48739c83155d
tahamahmoud7097-wq <tahamahmoud7097@gmail.com>
2026-01-24 20:24:55 +0200

    Test: add unit test module for repo.rs and a unit test for InMemoryRepo

--- src/repo.rs
+++ src/repo.rs
@@ -39,3 +39,23 @@ impl UrlRepo for InMemoryRepo {
         Ok(self.urls.pop_front())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{InMemoryRepo, UrlRepo, error::{Res, ResExt}};
+
+    #[tokio::test]
+    async fn test_inmemoryrepo() -> Res<()> {
+        let mut repo = InMemoryRepo::new().await;
+
+        for i in 0..50 {
+            repo.add(format!("https://example.com/index{}.html", i)).await.context("Failed to add URL to repo")?;
+        }
+
+        while let Some(url) = repo.pop().await.context("Failed to pop URL from repo")? {
+            println!("{}", url);
+        }
+
+        Ok(())
+    }
+}
